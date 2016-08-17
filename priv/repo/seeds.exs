aeeaead5ac8c810c1d08705b7139deb37f7ff566
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-16 21:44:45 -0500

    Seeds the db

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,32 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     GithubflowApi.Repo.insert!(%GithubflowApi.SomeModel{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule Sedder.Seed do
+  def make_prompt(data) do
+    %{id: id} = GithubflowApi.Repo.insert!(%GithubflowApi.Prompt{
+      text: data.prompt,
+      complete: 0 == length(data.children)
+    })
+
+    id
+  end
+  def make_from_tree(tree, parent_id) do
+    id = make_from_tree tree
+
+    GithubflowApi.Repo.insert!(%GithubflowApi.Answer{
+      text: tree.answer,
+      from_id: parent_id,
+      to_id: id
+    })
+  end
+
+  def make_from_tree(tree) do
+    id = make_prompt tree
+
+    for child <- tree.children, do: make_from_tree(child, id)
+
+    id
+  end
+end
+
+data = File.read!("data.json")
+  |> Poison.decode!(keys: :atoms)
+
+Sedder.Seed.make_from_tree data
