46a8aed6c743325ee62239c7354f3992517cf04c
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-16 21:59:31 -0500

    this isn't peepchat

--- config/prod.exs
+++ config/prod.exs
@@ -1,12 +1,12 @@
 use Mix.Config
 
-config :peepchat, GithubflowApi.Endpoint,
+config :githubflow_api, GithubflowApi.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "githubflow-decision-app.herokuapp.com", port: 443],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-config :peepchat, GithubflowApi.Repo,
+config :githubflow_api, GithubflowApi.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: 20
